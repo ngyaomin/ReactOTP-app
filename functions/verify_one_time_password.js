bdd7ba9acac468226fce38d9555c9ab85adcde7e
Yao Min <ngyaomin@ymail.com>
2017-06-28 15:49:44 +0800

    Finalize twilio setup

--- functions/verify_one_time_password.js
+++ functions/verify_one_time_password.js
@@ -6,18 +6,22 @@ module.exports = function(req, res) {
   }
 
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
-  const code = parseInt(code);
+  const code = parseInt(req.body.code);
 
   admin.auth().getUser(phone)
     .then(() => {
-      admin.database().ref('users/' + phone).on('value', snapshot => {
+      const ref = admin.database().ref('users/' + phone);
+      ref.on('value', snapshot => {
+        ref.off();
         const user = snapshot.val();
 
-        if (user.code !=== code || !user.codeValid) {
+        if (user.code !== code || !user.codeValid) {
           return res.status(422).send({ error: 'code is not valid' });
         }
 
-
+        ref.update({ codeValid: false });
+        admin.auth().createCustomToken(phone)
+          .then(token => res.send({ token: token }))
       });
     })
     .catch((err) => res.status(422).send({ error: err }))
